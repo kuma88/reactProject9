62768ef8f95ed762e6541a2a3e1b52d93c037735
kuma88 <kumarasond@gmail.com>
2020-12-23 13:41:13 +0800

    linked to firebase and subscribe & unsubscribe snapshot

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -10,13 +10,15 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import firebase from "../database/firebaseDB.js";
 import { GiftedChat } from "react-native-gifted-chat";
 
+const db = firebase.firestore().collection("messages");
 const auth = firebase.auth();
 
 export default function ChatScreen({ navigation }) {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    // This is the listener for authentication
+    const unsubscribeAuth = auth.onAuthStateChanged((user) => {
       if (user) {
         navigation.navigate("Chat");
       } else {
@@ -24,6 +26,7 @@ export default function ChatScreen({ navigation }) {
       }
     });
 
+    // This sets up the top right button
     navigation.setOptions({
       headerRight: () => (
         <TouchableOpacity onPress={logout}>
@@ -37,20 +40,42 @@ export default function ChatScreen({ navigation }) {
       ),
     });
 
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-    ]);
+    // setMessages([
+    //   {
+    //     _id: 2,
+    //     text: "Hello Too",
+    //     createdAt: new Date(),
+    //     user: {
+    //       _id: 1002,
+    //       name: "Another User",
+    //       avatar: "https://placeimg.com/141/140/any",
+    //     },
+    //   },
+    //   {
+    //     _id: 1,
+    //     text: "Hello developer",
+    //     createdAt: new Date(),
+    //     user: {
+    //       _id: 1001,
+    //       name: "React Native",
+    //       avatar: "https://placeimg.com/140/140/any",
+    //     },
+    //   },
+    // ]);
 
-    return unsubscribe;
+    // This unsubscribes snapshot and loads data from firebase
+    const unsubscribeSnapshot = db.orderBy("createdAt", "desc")
+    .onSnapshot((collectionSnapshot) => {
+      const serverMessages = collectionSnapshot.docs.map((doc) => doc.data());
+      setMessages(serverMessages);
+    });
+ 
+
+    //return unsubscribe;
+    return () => {
+      unsubscribeAuth();
+      unsubscribeSnapshot();
+    };
   }, []);
 
   function logout() {
@@ -58,16 +83,20 @@ export default function ChatScreen({ navigation }) {
   }
 
   function onSend(newMessages) {
-    setMessages([...newMessages, ...messages]);
+    console.log(newMessages);
+    const newMessage = newMessages[0];
+    db.add(newMessage);
+    //setMessages([...newMessages, ...messages]);
   }
 
   return (
     <GiftedChat
       messages={messages}
       onSend={(newMessages) => onSend(newMessages)}
+      renderUsernameOnMessage={true}
       listViewProps={{
         style: {
-          backgroundColor: "#666",
+          backgroundColor: "#427594",
         },
       }}
       user={{
